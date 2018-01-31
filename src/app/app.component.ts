44be9d5385703bc673ece54e372e0f7b60f66fc9
safari <rsafari.s@gmail.com>
2018-01-31 03:19:34 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,11 +44,11 @@ export class AppComponent {
       this.web3 = new Web3(window.web3.currentProvider);
     } else {
       console.warn(
-        'No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+        'No web3 detected. Falling back to http://localhost:7545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
       );
       // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
       this.web3 = new Web3(
-        new Web3.providers.HttpProvider('http://localhost:8545')
+        new Web3.providers.HttpProvider('http://localhost:7545')
       );
     }
   };
